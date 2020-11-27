0ca02fae90f57399d695e550a6c9f7be9b1276fe
raymoon-code <66543973+raymoon-code@users.noreply.github.com>
2020-11-27 01:21:32 -0600

    Update __main__.py

--- cathy/__main__.py
+++ cathy/__main__.py
@@ -22,20 +22,12 @@ def main():  # If called by entrypoint
     if '--help' in argv or '-h' in argv:
         print_usage()
         exit()
-    # dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(verbose=True)
-    env_path = Path('.') / '.env'
-    load_dotenv(dotenv_path=env_path)
-    # load_dotenv(find_dotenv())
-    DISCORD_TOKEN="NzgxMDUyOTM2Mjk3NTEyOTgw.X74COw.dACKthDv17mIgUEnkD36SMuyWUs"
-    DISCORD_CHANNEL="chat-with-bot"
-    DATABASE="/opt/cathy/db.sqlite3"
-
-    errors = []
+    
+    load_dotenv()
     environ['DISCORD_TOKEN'] = 'NzgxMDUyOTM2Mjk3NTEyOTgw.X74COw.dACKthDv17mIgUEnkD36SMuyWUs'
-    environ['DISCORD_CHANNEL'] = 'chat-with-bot'
-    environ['DATABASE'] ='NONE'
-
+    environ['DISCORD_CHANNEL']= "chat-with-bot"
+    environ['DATABASE'] = 'db.sqlite3'
+    errors = []
     if not os.getenv('DISCORD_TOKEN'):
         errors.append('No DISCORD_TOKEN found in environment variables.')
     if not os.getenv('DISCORD_CHANNEL'):
